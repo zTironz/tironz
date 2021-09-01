3ae4292db43f3d2feac55e304cfeb34cc9d281c6
Tiron <ole_g92@bk.ru>
2021-09-01 19:47:33 +0300

    edit cards js, add footer

--- app.js
+++ app.js
@@ -1,27 +1,29 @@
-const projCard = document.querySelector('.project__card');
-const imgBlack = document.querySelector('.project__card-img-black');
-const projImg = document.querySelector('.project__card-img');
-const projInfo = document.querySelector('.project__card-info');
-const projWrap = document.querySelector('.project__card-img-wrap');
-const link = document.querySelector('.project__card-link');
+const projCard = document.querySelectorAll('.project__card');
+const imgBlack = document.querySelectorAll('.project__card-img-black');
+const projImg = document.querySelectorAll('.project__card-img');
+const projInfo = document.querySelectorAll('.project__card-info');
+const projWrap = document.querySelectorAll('.project__card-img-wrap');
+const link = document.querySelectorAll('.project__card-link');
 const menuButton = document.querySelector('.header__menu-button-img');
 const popupMenu = document.querySelector('.popup__menu');
 const popupMenuClose = document.querySelector('.popup__menu-close');
 
+console.log(projCard)
 
-
-projCard.addEventListener('click', function(event){
-    if(event.target === imgBlack || event.target === projWrap) {
+projCard.forEach((el,i) => el.addEventListener('click', function(event){
+    if(event.target === imgBlack[i] || event.target === projWrap[i]) {
         console.log(event.target);
         console.log(this)
-        imgBlack.classList.toggle('hidden');
-        projImg.classList.toggle('hidden');
-        projInfo.classList.toggle('hidden');
-        link.classList.toggle('hidden');
-    }
-   
-    
-})
+        imgBlack[i].classList.toggle('hidden');
+        projImg[i].classList.toggle('hidden');
+        projInfo[i].classList.toggle('hidden');
+        link[i].classList.toggle('hidden');
+    }   
+}))
+
+
+
+
 menuButton.addEventListener('click', function(event){
     popupMenu.classList.add('popup__menu_active');
     if(event.target == popupMenuClose) {
